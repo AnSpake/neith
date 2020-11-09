e6e15829a0db2d1a9ba66a965a7077aa7468600f
Amandine Nassiri <amandine.nassiri@epita.fr>
2020-11-10 00:59:34 +0100

    neith: read from file

--- neith.sh
+++ neith.sh
@@ -4,10 +4,16 @@
 BINARY="$1"
 
 # Regex to use with grep to find every non-allowed boost symbols
-REGEX=" u |iterators|addressof|integral_constant"
-
 # 'grep -v " u "' is used to exclude every unique global symbol from our search
 # The result should be null
+REGEX=" u "
+
+# Read the symbols from given file and concatenate them to the REGEX variable
+while read -r line
+do
+    REGEX="${REGEX}|$line"
+done < symbols.txt
+
 RES=$(nm -C -g --defined-only "$BINARY" | grep boost | grep -Ev "$REGEX" | wc -l)
 
 if [ -z "$RES" ]
